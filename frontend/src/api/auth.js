517ba017ca0ba9143e8ff639d12e7c8fece398af
Apratim Dutta <2021itb089.apratim@students.iiests.ac.in>
2024-11-15 03:07:17 +0530

    integration API

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://car-management-web-app.onrender.com/api';
+const API_URL =  'https://car-management-web-app.onrender.com/api';
 
 export const register = async (userData) => {
   const response = await axios.post(`${API_URL}/auth/create-user`, userData);
